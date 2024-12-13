1ee2a3cb6987954748a58cdb6c96e0614987e053
Shotabdi Rongdi <145760914+shotabdi12@users.noreply.github.com>
2024-12-14 00:35:20 +0600

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    private final String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
