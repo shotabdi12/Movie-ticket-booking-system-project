79ebc8c392ae583fc6c565ae09833158bf65f040
Shotabdi Rongdi <145760914+shotabdi12@users.noreply.github.com>
2024-12-14 00:35:01 +0600

    Add files via upload

---
+++ Theater.java
@@ -0,0 +1,18 @@
+public class Theater {
+    private final String name;
+    private final String location;
+
+    public Theater(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
+
