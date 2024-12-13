ea8fc4d3b618ea1803f06b604cc8a6892362c67e
Shotabdi Rongdi <145760914+shotabdi12@users.noreply.github.com>
2024-12-13 06:01:46 +0600

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,21 @@
+
+public class Customer {
+
+    private final String name;
+    private final int age;
+
+    // Constructor
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
